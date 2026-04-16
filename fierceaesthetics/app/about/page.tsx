c957b421f494913a86009770ba79737f659db104
aawhittle <89033382+aawhittle@users.noreply.github.com>
2026-04-16 13:09:00 +0100

    Update page.tsx

--- fierceaesthetics/app/about/page.tsx
+++ fierceaesthetics/app/about/page.tsx
@@ -1,23 +1,22 @@
 export default function AboutPage() {
   return (
-    <div className="space-y-10 max-w-3xl">
-
-      <h1 className="text-5xl font-medium">
-        About Me
-      </h1>
+   <section className="px-6 py-12">
+      <div className="max-w-2xl mx-auto space-y-6">
 
       <div
         className="h-1 w-32 rounded"
         style={{ backgroundColor: "rgb(var(--gold))" }}
       />
 
-      <p className="text-neutral-600 text-lg">
-      Welcome to Fierce Aesthetics.
+      <p className="text-neutral-600 text-lg font-medium">
+          Welcome to <span className="font-semibold">Fierce Aesthetics</span>.
       </p>
 
       <p className="text-neutral-600">
-      I’m Emma, the face behind the brand, dedicated to helping you feel confident in your own skin.\n
-      \n
+      I’m Emma, the face behind the brand, dedicated to helping you feel confident in your own skin. 
+      </p>
+
+      <p className="text-neutral-600">
       With over ten years experience in the beauty industry, I have trained with some of the industry’s most respected and award-winning brands, including Finishing Touches and Million Dollar Facial. 
       This has allowed me to develop a highly refined, results led approach, focused on delivering exceptional standards and visible, lasting results.
       </p>
