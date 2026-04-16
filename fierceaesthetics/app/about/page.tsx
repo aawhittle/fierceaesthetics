77d52447ff36d6ac91ddab9ffbb483e5f022ef29
aawhittle <89033382+aawhittle@users.noreply.github.com>
2026-04-16 13:27:12 +0100

    Update page.tsx

--- fierceaesthetics/app/about/page.tsx
+++ fierceaesthetics/app/about/page.tsx
@@ -26,6 +26,33 @@ export default function AboutPage() {
       This has allowed me to develop a highly refined, results led approach, focused on delivering exceptional standards and visible, lasting results.
       </p>
 
+       <p className="text-neutral-600">
+      My focus is on enhancing your natural beauty through subtle, sophisticated treatments that leave you looking 
+      refreshed, sculpted, and effortlessly confident, never overdone.
+      </p>
+
+       <p className="text-neutral-600">
+      My expertise includes industry leading treatments such as Million Dollar facials, Million Dollar micro-needling, Immaculate peels, 
+      Oxy Pro and Clay Mask facials, alongside advanced body treatments using radio frequency and cavitation designed to designed to sculpt, tone, and enhance overall skin quality.  
+      I also offer precision led treatments such as the Korean Lash Lift and Brow Lamination, ideal for creating a polished, low maintenance look that enhances your natural features.
+      </p>
+
+       <p className="text-neutral-600">
+      From skin rejuvenation to body sculpting and contouring, every treatment is carefully tailored to you. 
+      Whether you are preparing for a holiday, a special event, or simply investing in yourself, 
+      each experience is designed to deliver polished, confidence boosting results.
+      </p>
+
+        <p className="text-neutral-600">
+      Fierce Aesthetics was created to offer a calm, luxurious space where you can switch off from the 
+      outside world and feel completely looked after. I pride myself on honest advice, attention to detail 
+      and creating bespoke treatment plans that align with your goals and lifestyle.
+      </p>
+
+         <p className="text-neutral-600">
+    For my clients, this is more than maintenance, it’s an investment in confidence, self care 
+    and stepping into your most polished self.
+    </p>
     </div>
   </section>
   )
